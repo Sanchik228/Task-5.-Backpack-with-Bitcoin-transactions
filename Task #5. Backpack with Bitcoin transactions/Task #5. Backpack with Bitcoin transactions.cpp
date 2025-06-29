ed85ba82aa79d7e5297fed785a1ebd696d42b338
Sanchik228 <Oleksandra.Slastikhina@cs.khpi.edu.ua>
2025-06-29 19:53:52 +0300

    finale

--- Task #5. Backpack with Bitcoin transactions/Task #5. Backpack with Bitcoin transactions.cpp
+++ Task #5. Backpack with Bitcoin transactions/Task #5. Backpack with Bitcoin transactions.cpp
@@ -4,6 +4,10 @@
 #include <vector>
 #include <string>
 #include <algorithm>
+#include <chrono>
+#include <iomanip>
+
+const int MAX_BLOCK_SIZE = 1000000;
 
 struct Transaction {
     std::string id;
@@ -20,10 +24,10 @@ std::vector<Transaction> readTransaction(const std::string& f) {
     std::ifstream file(f);
     std::string line;
 
-        if (!file) {
-            std::cerr << "Cannot open file: " << f << std::endl;
-            return transactions;
-        }
+    if (!file) {
+        std::cerr << "Cannot open file: " << f << std::endl;
+        return transactions;
+    }
 
     while (std::getline(file, line)) {
         std::stringstream ss(line);
@@ -35,7 +39,7 @@ std::vector<Transaction> readTransaction(const std::string& f) {
                 int fee = std::stoi(fee_str);
                 transactions.push_back({ id, size, fee });
             }
-            catch(const std::exception& e){
+            catch (const std::exception& e) {
                 std::cerr << "Error parsing line: " << line << std::endl;
             }
         }
@@ -47,7 +51,30 @@ int main() {
     std::string filename = "transactions.csv";
     auto transactions = readTransaction(filename);
 
-    std::cout << "Read transactions: " << transactions.size() << std::endl;
+    int cBlockSize = 0, totalFee = 0;
+
+    std::vector<Transaction> block;
+    auto start = std::chrono::high_resolution_clock::now();
+
+    std::sort(transactions.begin(), transactions.end(), [](const Transaction& a, const Transaction& b) {
+        return a.feePerByte() > b.feePerByte();
+        });
+
+    for (const auto& tx : transactions) {
+        if (cBlockSize + tx.size <= MAX_BLOCK_SIZE) {
+            block.push_back(tx);
+            cBlockSize += tx.size;
+            totalFee += tx.fee;
+        }
+    }
+
+    auto end = std::chrono::high_resolution_clock::now();
+    auto elapsed_us = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
+
+    std::cout << "Block constructed!\n" << "Transactions in block : " << block.size() << std::endl
+        << "Block size: " << cBlockSize << " bytes\n" << "Total fee: " << totalFee << " satoshis\n"
+        << "Construction time: " << elapsed_us.count() << " ms" << std::endl
+        << "Read transactions: " << transactions.size() << std::endl;
 
     return 0;
 }
